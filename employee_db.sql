ecc6029644467155bfebb62a444720940e281108
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-03-18 23:49:46 -0400

    department

--- employee_db.sql
+++ employee_db.sql
@@ -3,20 +3,19 @@ CREATE database employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  song VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
+CREATE TABLE department ( 
+-- id - INT PRIMARY KEY
+-- name - VARCHAR(30) to hold department name
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
+    -- id - INT PRIMARY KEY
+    -- title -  VARCHAR(30) to hold role title
+    -- salary -  DECIMAL to hold role salary
+    -- department_id -  INT to hold reference to department role belongs to
   position INT NOT NULL,
   artist VARCHAR(100) NULL,
   album VARCHAR(100) NULL,
@@ -30,6 +29,11 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
+--     id - INT PRIMARY KEY
+    -- first_name - VARCHAR(30) to hold employee first name
+    -- last_name - VARCHAR(30) to hold employee last name
+    -- role_id - INT to hold reference to role employee has
+    -- manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
   position INT NOT NULL,
   artist VARCHAR(100) NULL,
   album VARCHAR(100) NULL,
