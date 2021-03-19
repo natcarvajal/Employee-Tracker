341a0ba48c017591b21aa1103e552febf65f9c7a
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-03-18 23:55:56 -0400

    employee/role setup

--- employee_db.sql
+++ employee_db.sql
@@ -11,21 +11,16 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     -- id - INT PRIMARY KEY
     -- title -  VARCHAR(30) to hold role title
     -- salary -  DECIMAL to hold role salary
     -- department_id -  INT to hold reference to department role belongs to
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  album VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
@@ -34,16 +29,12 @@ CREATE TABLE employee (
     -- last_name - VARCHAR(30) to hold employee last name
     -- role_id - INT to hold reference to role employee has
     -- manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  album VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+  PRIMARY KEY (id)
 );
 
 SELECT * FROM top5000;
