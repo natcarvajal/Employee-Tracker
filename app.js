cf1636c467a238283fb7b0717d6cacecab60377e
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-03-23 15:39:29 -0400

    added image for github

--- app.js
+++ app.js
@@ -78,87 +78,25 @@ function viewD() {
     runSearch();
   });
 }
-// inquirer
-//   .prompt({
-//     name: "View Department",
-//     type: "input",
-//     message: "Please select what you would like to add?",
-//   })
-//   .then(function (answer) {
-//     var query = "SELECT position, song, year FROM top5000 WHERE ?";
-//     connection.query(query, { artist: answer.artist }, function (err, res) {
-//       for (var i = 0; i < res.length; i++) {
-//         console.log(
-//           "Position: " +
-//             res[i].position +
-//             " || Song: " +
-//             res[i].song +
-//             " || Year: " +
-//             res[i].year
-//         );
-//       }
-//       runSearch();
-//     });
-//   });
 
 function viewR() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function (err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
+  console.log("Viewing all roles..");
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+}
+function viewE() {
+  console.log("Viewing all employees..");
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
     runSearch();
   });
 }
 
-function viewE() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function (value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        },
-      },
-    ])
-    .then(function (answer) {
-      var query =
-        "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
-
-function songSearch() {
+function addD() {
   inquirer
     .prompt({
       name: "song",
